405fddc75e3df65d0420f1208d3c9c6c2b1a5f7a
n-nishizawa <n-nishizawa@nekojarashi.com>
2015-05-14 12:02:28 +0900

    Add servo y axis controll

--- obj_tracker.rb
+++ obj_tracker.rb
@@ -17,6 +17,14 @@ arduino.on :analog_read do |pin, value| # analog_read event
 
     arduino.servo_write 3, angle
   end
+
+  if pin == 1
+    angle = Ot::Utils.map(value, 0, 1023, 0, 180)
+    puts "analog pin #{pin} changed : #{value} #{angle}"
+
+    arduino.servo_write 5, angle
+  end
+
 end
 
 loop do 
